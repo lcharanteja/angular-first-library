42acbb233a338933a1fe1c1c070c33f414f2cdb3
Charan Teja Lellaboyena <charan@Charans-MBP-2.fios-router.home>
2020-01-11 16:37:17 -0500

    added angular and flex-layouts to library

---
+++ projects/first-lib/src/lib/common-dialog/common-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-common-dialog',
+  templateUrl: './common-dialog.component.html',
+  styleUrls: ['./common-dialog.component.css']
+})
+export class CommonDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
